81c15c0ea104c7a2ba76741da0d00e0ca8be391b
Nick Yahnke <nick@pitchinteractive.com>
2022-12-04 14:40:37 -0800

    use host properly in frontend

--- frontend/src/corsFetch.js
+++ frontend/src/corsFetch.js
@@ -1,5 +1,6 @@
 import defaultServerPort from './defaultServerPort';
-const corsServer = `http://localhost:${defaultServerPort}`;
+const host = process.env.REACT_APP_HOST || 'localhost';
+const corsServer = `http://${host}:${defaultServerPort}`;
 
 export default function corsFetch(url, options) {
   const corsUrl = `${corsServer}/cors?url=${encodeURIComponent(url)}`;
